db37bc60c77b75bd94c150dbd6ad9466914dee95
mamun-prodhan <mamunpr9@gmail.com>
2023-10-08 00:47:37 +0600

    home page layout added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
+import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
+import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
+
 const Home = () => {
   return (
     <div>
+      <Header></Header>
+      <Navbar></Navbar>
       <h2 className="text-3xl font-poppins font-bold">this is home page</h2>
+      <div className="grid grid-cols-1 md:grid-cols-4">
+        <div>
+          <LeftSideNav></LeftSideNav>
+        </div>
+        <div className="col-span-2">
+          <h2 className="text-4xl">News Comming Soon</h2>
+        </div>
+        <div>
+          <RightSideNav></RightSideNav>
+        </div>
+      </div>
     </div>
   );
 };
