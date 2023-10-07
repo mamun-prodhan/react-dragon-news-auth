cd3734c857276d674e2d00e3954ee169e3a15e49
mamun-prodhan <mamunpr9@gmail.com>
2023-10-07 23:51:13 +0600

    custom font family added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h2 className="text-3xl">this is home page</h2>
+      <h2 className="text-3xl font-poppins font-bold">this is home page</h2>
     </div>
   );
 };
