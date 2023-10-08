292f7cd186ed52fc1f0ff64a1d6e8fd28cd1f910
mamun-prodhan <mamunpr9@gmail.com>
2023-10-08 11:00:58 +0600

    category added

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
-    <div>
-      <h2 className="text-2xl">LeftSideNav</h2>
+    <div className="space-y-3">
+      <h2 className="text-xl font-semibold">
+        All Category {categories.length}
+      </h2>
+      {categories.map((category) => (
+        <button
+          className="block btn btn-ghost w-full text-start"
+          key={category.id}
+        >
+          <Link to={`/category/${category.id}`}>{category.name}</Link>
+        </button>
+      ))}
     </div>
   );
 };
